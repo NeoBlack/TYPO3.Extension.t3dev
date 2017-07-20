8b9b5209c5683ae871047356a3736d8836e65037
Frank Naegler <frank.naegler@typo3.org>
2017-07-20 20:30:06 +0200

    [TASK] First import

---
+++ ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "t3dev".
+#
+# Auto generated 08-08-2015 04:01
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'T3dev',
+	'description' => 'links for developer from the good old extdeveval extension and some more links - but only for TYPO3 4.2 up. new developer functions: FlexForm-Generator.',
+	'category' => 'be',
+	'shy' => 0,
+	'version' => '1.1.5',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'TYPO3_version' => '4.2.0-4.4.99',
+	'PHP_version' => '5.1.0-0.0.0',
+	'module' => '',
+	'state' => 'beta',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Frank NÃÂ¤gler',
+	'author_email' => 'typo3@naegler.net',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+);
+
+?>
